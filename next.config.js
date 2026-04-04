f3c421d55f018c0dd014b6647cc41661ca8fa7c1
RoshniYadav05 <roshni022004@gmail.com>
2026-04-04 19:35:18 +0530

    remove dynamic ssr false

--- next.config.js
+++ next.config.js
@@ -1,11 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  images: {
-    // Domains for next/image to allow external image loading
-    domains: ["placehold.co", "jdowuzdumrucojkramsx.supabase.co"],
 
-    // Optional: keep remotePatterns if you want pattern-based image loading
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+
+  images: {
+    domains: ["placehold.co", "jdowuzdumrucojkramsx.supabase.co"],
     remotePatterns: [
       {
         protocol: "https",
